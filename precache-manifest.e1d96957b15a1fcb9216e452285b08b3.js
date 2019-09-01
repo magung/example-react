be1c39c47f6f068158b28dafb192e6555273dae6
Agung Maulana 73 <agungbinunju040100@gmail.com>
2019-09-01 23:34:03 +0700

    Updates

---
+++ precache-manifest.e1d96957b15a1fcb9216e452285b08b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "474c61b0dd6fdea00fe4b5b62bbe2a5f",
+    "url": "/magung/example-react/index.html"
+  },
+  {
+    "revision": "6b897023cf214f13c578",
+    "url": "/magung/example-react/static/css/main.52097e55.chunk.css"
+  },
+  {
+    "revision": "a30ac0cfaf0a65d0a751",
+    "url": "/magung/example-react/static/js/2.0dad5db2.chunk.js"
+  },
+  {
+    "revision": "6b897023cf214f13c578",
+    "url": "/magung/example-react/static/js/main.2ec69c46.chunk.js"
+  },
+  {
+    "revision": "fda7d26a3cf54fa7d210",
+    "url": "/magung/example-react/static/js/runtime~main.0f7daacd.js"
+  }
+]);
